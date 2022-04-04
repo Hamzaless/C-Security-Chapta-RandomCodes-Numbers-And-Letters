5079eb458a2de9a96390ca502f348f4ac675adc7
hamzampbstars <89343438+hamzampbstars@users.noreply.github.com>
2022-04-04 17:11:23 +0300

    Proje dosyası ekle.

---
+++ Security Chapta/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Security_Chapta
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(95, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "{``Chapta Code``}";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(20, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Enter Chapta Code";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.button1.Location = new System.Drawing.Point(227, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Verify";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Skip Chapta (Delete Me) ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-8, 141);
+            this.progressBar1.MarqueeAnimationSpeed = 1000;
+            this.progressBar1.Maximum = 300;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(444, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(425, 151);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Chapta Security";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Security Chapta/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Diagnostics;
+
+namespace Security_Chapta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TopMost = true;
+
+            Random rastgele = new Random();
+            Random rastgelesayi = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                
+                string harfler = "ABCDEFGHIJKLMNOPRSTUVYZabcdefghIijklmnoprstuvyz";
+                string uret = "";
+                for (int a= 0; a < 3; a++)
+                {
+                    uret += harfler[rastgele.Next(harfler.Length)];
+                }
+                int sayi = rastgele.Next(1, 50);
+               
+                label1.Text = sayi.ToString();
+
+                int sayi2 = rastgele.Next(1, 50);
+                label1.Text = sayi.ToString() + sayi2.ToString() ;
+
+                int sayi3 = rastgele.Next(1, 50);
+                label1.Text = sayi.ToString() + sayi2.ToString()+sayi3.ToString();
+                int sayi4 = rastgele.Next(1, 50);
+                label1.Text = sayi.ToString() + sayi2.ToString() + sayi3.ToString() + sayi4.ToString() ;
+                int sayi5 = rastgele.Next(1, 50);
+                label1.Text = sayi.ToString() +uret+ sayi2.ToString() + sayi3.ToString() + sayi4.ToString() +uret+ sayi5.ToString();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           if (textBox1.Text==label1.Text)
+            {
+                Application.Exit();
+                
+            }
+           else
+            {
+                MessageBox.Show("Enter Chapta Code or You Can't Exit", "Chapta Closing Error");
+                e.Cancel = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+        if (textBox1.Text == label1.Text)
+            {
+
+            }
+        else
+            {
+                foreach (Process proc in Process.GetProcessesByName("taskmgr"))
+                {
+                    proc.Kill();
+                    MessageBox.Show("You can't do this without going past the chapta: Task Manager Blocker", proc.ProcessName);
+                }
+                foreach (Process proc2 in Process.GetProcessesByName("cmd"))
+                {
+                    proc2.Kill();
+                    MessageBox.Show("You can't do this without going past the chapta: Command Prompt Blocker", proc.ProcessName);
+                }
+            }
+           
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = label1.Text;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(3);
+            if (progressBar1.Value == 300)
+            {
+                if (textBox1.Text == label1.Text)
+                {
+                    timer2.Stop();
+                    MessageBox.Show("Chapta Passed", "Chapta");
+                    Application.Exit();
+                }
+                else
+                {
+                    timer2.Stop();
+                    MessageBox.Show("Chapta Code Incorrect", "Chapta Error"); 
+                    progressBar1.Value = 0;
+                }
+            }
+        }
+    }
+}
